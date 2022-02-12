89e8d606549a8afc98099a4427a84fcabdb46607
jnghg <eniows@naver.com>
2022-02-12 11:02:36 +0900

    로그인화면 구성

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  //darkMode: "media" -- 컴퓨터 환경설정에 따름 or "class" 브라우저에서 직접설정 필요
+  plugins: [require("@tailwindcss/forms")],
 };
